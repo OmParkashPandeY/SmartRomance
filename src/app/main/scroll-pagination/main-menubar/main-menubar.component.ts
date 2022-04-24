ce5a7df1f15f317b1c52d897aebf307294b35ce9
Om Parkash PandeY <ommanglamak47@gmail.com>
2022-04-24 14:43:26 +0530

    Add timeout for smooth ui

--- src/app/main/scroll-pagination/main-menubar/main-menubar.component.ts
+++ src/app/main/scroll-pagination/main-menubar/main-menubar.component.ts
@@ -11,14 +11,15 @@ export class MainMenubarComponent implements OnInit {
 
      public lengthOfCommentList: number = 0;
      public paginationIndex: number = 0;
+     public isShowSpinner: boolean = false;
      constructor() {
           this.commentList = new DummyComments();
           this.lengthOfCommentList = this.commentList.commentsList.length;
      }
 
      ngOnInit(): void {
-          if (this.lengthOfCommentList > 50) {
-               this.paginationIndex = 50;
+          if (this.lengthOfCommentList > 25) {
+               this.paginationIndex = 25;
           } else {
                this.paginationIndex = this.lengthOfCommentList;
           }
@@ -27,12 +28,15 @@ export class MainMenubarComponent implements OnInit {
      onScroll(event: any) {
           if (event.target.offsetHeight + event.target.scrollTop >= event.target.scrollHeight - 5) {
                if (this.lengthOfCommentList > this.paginationIndex) {
+                    this.isShowSpinner = true;
                     setTimeout(() => {
-                         this.paginationIndex = this.paginationIndex + 50;
+                         this.paginationIndex = this.paginationIndex + 25;
+                         this.isShowSpinner = false;
                     }, 1000);
                } else {
                     setTimeout(() => {
                          this.paginationIndex = this.lengthOfCommentList;
+                         this.isShowSpinner = false;
                     }, 1000);
                }
           }
